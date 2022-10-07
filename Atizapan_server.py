8b9c0a391afb6143cab21de68cfaed79eb766db7
facebooker212 <facebooker3124@yahoo.com>
2022-10-06 23:01:35 -0500

    Added mongodb support

--- Atizapan_server.py
+++ Atizapan_server.py
@@ -1,68 +1,55 @@
 from flask import Flask, request, render_template
+from pymongo import MongoClient
+from dotenv import load_dotenv
+from bson import json_util
+import os
+import urllib
+import json
 
 app = Flask(__name__)
+
+client = MongoClient()
+
+load_dotenv()
+
+mongouri = "mongodb://Admin:" + urllib.parse.quote_plus(os.environ["PSSWD"]) + "@127.0.0.1:27017/"
+client = MongoClient(mongouri)
+
+db = client['atizapanCoords']
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/subnoti')
-def subnoti():
-    return render_template('noti_submit.html')
-
-@app.route('/subnoti', methods=['POST'])
-def postnoti():
-    noti = request.form['notis']
-    f = open("notis.txt", "w")
-    f.write(noti)
-    f.close()
-    return "notificaciones actualizadas"
-
-@app.route('/notis')
-def readnotis():
-    f = open("notis.txt", "r")
-    n = f.read()
-    return n
-
 @app.route('/subcoord')
 def subcoord():
     return render_template('coord_submit.html')
 
+
 @app.route('/subcoord', methods=['POST'])
 def postcoord():
-    coords = request.form['coords']
-    f = open("coords.txt", "r")
-    c = f.read()
-    f.close()
-    if (len(checklen(c)) == 1):
-        f = open("coords.txt", "w")
-        f.write(coords)
-    elif (len(checklen(c)) == 20):
-        nc = checklen(c)
-        del nc[0]
-        del nc[0]
-        sc = ", ".join(nc)
-        f = open("coords.txt", "w")
-        f.write(sc + ", " + coords)
-    else:
-        f = open("coords.txt", "w")
-        f.write(c + ", " + coords)
-    f.close()
-    return "coordenadas actualizadas"
+    if request.form['b_coords'] == "Subir coordenadas":
+        coords = request.form['coords']
+        db.atizapanCoords.update_one({"Coords":{"$exists": True}}, {"$push":{"Coords":coords}})
+    elif request.form['b_coords'] == "Borrar primer coordenada":
+        db.atizapanCoords.update_one({"Coords":{"$exists": True}}, {"$pop":{"Coords":-1}})
+    return "Coordenadas actualizadas"
 
 @app.route('/coords')
 def readcoords():
-    f = open("coords.txt", "r")
-    c = f.read()
-    return c
+    data = db.atizapanCoords.find({"Coords":{"$exists": True}})
+    coord_dict = ""
+    for a in data:
+        coord_dict = a["Coords"]
+    #json_docs = []
+    #for doc in data:
+    #    json_doc = json.dumps(doc, default=json_util.default)
+    #    json_docs.append(json_doc)
+    #print(json_docs["Coords"])
+    return coord_dict
 
-def checklen(s):
-    l = s.split(",")
-    print(l)
-    print(len(l))
-    return l
 
 if __name__ == '__main__':
     from waitress import serve
-    #app.run(use_reloader=True, port=5000, threaded=True)
-    serve(app, host="0.0.0.0", port=5000, url_scheme='https')
+    app.run(use_reloader=True, port=5000, threaded=True)
+    #serve(app, host="0.0.0.0", port=5000, url_scheme='https')
